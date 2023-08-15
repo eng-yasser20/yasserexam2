ffb0b677bd427dfa26080016dd5278d719225c24
eng-yasser20 <140945901+eng-yasser20@users.noreply.github.com>
2023-08-15 15:13:18 +0300

    Add files via upload

---
+++ nu.js
@@ -0,0 +1,12 @@
+clickableItem_beef = document.getElementById('beef');
+clickableItem_chicken = document.getElementById('chicken');
+clickableItem_sushi = document.getElementById('sushi');
+
+clickableItem_beef.addEventListener('click', function() {
+ window.location.href="beef.html"});
+
+clickableItem_chicken.addEventListener('click', function() {
+ window.location.href="chiken.html"});
+
+ clickableItem_sushi.addEventListener('click', function() {
+     window.location.href = "sushi.html"});
